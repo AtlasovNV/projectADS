4fd2904a2b004d02ee9af008db4e3cf949092676
evlx87 <evilnyagaming@yandex.ru>
2019-11-17 09:26:42 +0300

    add UserLoginForm, modify def login/logout

--- CampaignDesigner/authapp/views.py
+++ CampaignDesigner/authapp/views.py
@@ -1,10 +1,27 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from authapp.forms import UserLoginForm
 
 
-# Create your views here.
 def login(request):
-    return request
+    title = 'вход'
+    login_form = UserLoginForm(data=request.POST)
+
+    if request.method == 'POST' and login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main'))
+
+    context = {'title': title, 'login_form': login_form}
+    return render(request, 'authapp/login.html', context)
 
 
 def logout(request):
-    return request
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main'))
