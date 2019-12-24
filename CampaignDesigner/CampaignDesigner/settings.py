9defc78bb4d4f8f85371b3341cef0af0971edd12
sab1e <antony.sobolev@gmail.com>
2019-12-24 23:23:28 +0500

    change setting for deploy

--- CampaignDesigner/CampaignDesigner/settings.py
+++ CampaignDesigner/CampaignDesigner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g&+#ebgavt+dyh^lz_d8yg(_73lpjw2e-f#k78f6xxwobk-dq3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'CampaignDesigner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projectads',
+        'USER': 'projectads',
+        'PASSWORD': 'geekbrains',
+        'HOST': 'localhost'
     }
 }
 
